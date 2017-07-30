50094644bc4d03d4f0901d6989f42a06d702fe6b
jgorsica <jgorsica@users.noreply.github.com>
2017-07-30 11:55:11 -0500

    Update neorotate.py

--- neorotate.py
+++ neorotate.py
@@ -230,6 +230,6 @@ if __name__ == '__main__':
       theta_to_pass.value = int(get_theta(sensor_data))
     else:
       theta_to_pass.value = -1
-    time.sleep(0.1)
+    time.sleep(0.025)
   for process in processes:
       process.join()
