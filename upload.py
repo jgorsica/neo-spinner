ebdf2b79e1bdc83582b5e116f8e8627152ecf9be
jgorsica <jgorsica@users.noreply.github.com>
2017-08-06 12:12:00 -0500

    Update upload.py

--- upload.py
+++ upload.py
@@ -2,5 +2,5 @@ import requests
 import sys
 
 url = "http://192.168.1.66:5000/upload"
-args = {'file': open(sys.argv[1], 'rb'), 'filename':sys.argv[1]}
-r = requests.post(url, args=args)
+files = {'file': (sys.argv[1],open(sys.argv[1], 'rb'))}
+r = requests.post(url, files=files)
