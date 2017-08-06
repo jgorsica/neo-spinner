08791950be1c1320251a67364a33e0ab0f1599c5
jgorsica <jgorsica@users.noreply.github.com>
2017-08-06 11:10:52 -0500

    Create spinner_server.py

---
+++ spinner_server.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import request
+import time
+import neorotate
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['files']
+    filename='pic.png'
+		f.save(filename)
+		show_it(filename)
+		return '200'
+	else:
+		return 'Upload Page'
+
+def show_it():
+	exec(open("./neorotate.py").read(),filename)
+
+if __name__ == '__main__':
+	app.debug = False
+	app.run(host='0.0.0.0')
+  
